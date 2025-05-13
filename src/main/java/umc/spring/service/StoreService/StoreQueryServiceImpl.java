7de6c8c43d7c6674e728100e5919e00ee36a5b24
junekyu02 <baekjunekyu02@gmail.com>
2025-05-13 15:38:41 +0900

    mission/#7

--- src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.domain.Store;
-import umc.spring.repository.StoreRepository.StoreRepository;
+import umc.spring.repository.StoreRepository;
 
 
 import java.util.List;
