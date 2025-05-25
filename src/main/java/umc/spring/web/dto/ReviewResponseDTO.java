88e5dbad7d06a1b98d6f3c2d8335d8a917ec2973
2ghrms <aa020228@gmail.com>
2025-05-25 20:23:46 +0900

    practice/#09

--- src/main/java/umc/spring/web/dto/ReviewResponseDTO.java
+++ src/main/java/umc/spring/web/dto/ReviewResponseDTO.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 public class ReviewResponseDTO {
 
@@ -15,6 +17,30 @@ public class ReviewResponseDTO {
     @AllArgsConstructor
     public static class CreateReviewResultDTO {
         Long reviewId;
+        LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO {
+        List<ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO {
+        String ownerNickname;
+        Float rate;
+        String content;
         LocalDateTime createdAt;
     }
 }
